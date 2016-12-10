77acce63d4deb7fff68f25d84e9e1ed0cc3773c3
Volcomix <volcomix@gmail.com>
2016-12-10 18:32:49 +0100

    Add move method

--- physics/body.py
+++ physics/body.py
@@ -2,3 +2,6 @@ from ..geometry.circle import *
 
 class Body(namedtuple('Body', Circle._fields + ('speed', 'mass'))):
     __slots__ = ()
+
+    def move(self):
+        return self._replace(position=self.position + self.speed)
