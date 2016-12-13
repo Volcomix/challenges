a424cbe7d72bdbf2154ceb14a165ba7753378c1e
Volcomix <volcomix@gmail.com>
2016-12-14 00:07:27 +0100

    Init body to circle collision detection

--- physics/body.py
+++ physics/body.py
@@ -13,3 +13,37 @@ class Body(namedtuple('Body', Circle._fields + ('speed', 'mass'))):
 
     def friction(self, value):
         return self._replace(speed=self.speed * value)
+    
+    def will_collide_circle(self, circle):
+        dist = self.position.dist(circle.position)
+        sumRadii = self.radius + circle.radius
+        dist -= sumRadii
+        if self.speed.length < dist:
+            return False
+        
+        N = self.speed.norm()
+        C = circle.position - self.position
+        D = N.dot(C)
+
+        if D <= 0:
+            return False
+        
+        lengthC = C.length
+        F = lengthC * lengthC - D * D
+
+        sumRadiiSquared = sumRadii * sumRadii
+        if F >= sumRadiiSquared:
+            return False
+        
+        T = sumRadiiSquared - F
+
+        if T < 0:
+            return False
+        
+        distance = D - math.sqrt(T)
+        mag = self.speed.length
+
+        if mag < distance:
+            return False
+        
+        return True
