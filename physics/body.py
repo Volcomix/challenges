72e54bbe579bcd504cb03258bd4cdb8ac4dca54d
Volcomix <volcomix@gmail.com>
2016-12-15 07:51:44 +0100

    Fix checkpoint detection

--- physics/body.py
+++ physics/body.py
@@ -15,11 +15,11 @@ class Body(namedtuple('Body', Circle._fields + ('speed', 'mass'))):
 
     def friction(self, value):
         return self._replace(speed=self.speed * value)
-    
+
     def will_collide_circle(self, circle):
         dist = self.position.dist(circle.position)
-        sumRadii = self.radius + circle.radius
-        dist -= sumRadii
+        sum_radii = self.radius + circle.radius
+        dist -= sum_radii
         if self.speed.length < dist:
             return False
         
@@ -30,14 +30,47 @@ class Body(namedtuple('Body', Circle._fields + ('speed', 'mass'))):
         if D <= 0:
             return False
         
-        lengthC = C.length
-        F = lengthC * lengthC - D * D
+        length_c = C.length
+        F = length_c * length_c - D * D
 
-        sumRadiiSquared = sumRadii * sumRadii
-        if F >= sumRadiiSquared:
+        sum_radii_squared = sum_radii * sum_radii
+        if F >= sum_radii_squared:
             return False
         
-        T = sumRadiiSquared - F
+        T = sum_radii_squared - F
+
+        if T < 0:
+            return False
+        
+        distance = D - math.sqrt(T)
+        mag = self.speed.length
+
+        if mag < distance:
+            return False
+        
+        return True
+
+    def will_reach_circle(self, circle):
+        dist = self.position.dist(circle.position)
+        dist -= circle.radius
+        if self.speed.length < dist:
+            return False
+        
+        N = self.speed.norm()
+        C = circle.position - self.position
+        D = N.dot(C)
+
+        if D <= 0:
+            return False
+        
+        length_c = C.length
+        F = length_c * length_c - D * D
+
+        radius_squared = circle.radius * circle.radius
+        if F >= radius_squared:
+            return False
+        
+        T = radius_squared - F
 
         if T < 0:
             return False
